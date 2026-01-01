2c0f80596e1ffe4e843e41ce0524815df46fcc74
Hudson Ross <hudson@sejongconsortium.com>
2026-01-01 14:14:23 -0800

    Removed book path hardcoding in favour of parameter.

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
-from stats import char_count
+import sys
+
+from stats import char_count, num_key, word_count, sort_on, report
 
 def main():
-    num_words = char_count("./books/frankenstein.txt")
-    print(num_words)
+
+    if (len(sys.argv) < 2):
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    book_path = str(sys.argv[1])
+    num_chars = char_count(book_path)
+    num_words = word_count(book_path)
+    sorted_counts = sort_on(num_chars, num_key)
+    report(sorted_counts, book_path, num_words)
 
 main()
--- stats.py
+++ stats.py
@@ -10,15 +10,45 @@ def word_count(filepath):
 
 
 def char_count(filepath):
-    char_dict = {}
+    list_of_dict = []
+    
     text = get_book_text(filepath)
     text_lower = text.lower()
     chars = list(text_lower)
-    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    alphabet = "abcdefghijklmnopqrstuvwxyzâêëô"
     for current_letter in alphabet:
+        current_char_dict = {}
         current_letter_list = []
         current_letter_list = [current_letter for char in chars if char == current_letter]
-        char_dict[current_letter] = len(current_letter_list)
-    return char_dict
+        char_dict_char_pref = "char"
+        char_dict_num_pref = "num"
+        current_char_dict[char_dict_char_pref] = current_letter
+        current_char_dict[char_dict_num_pref] = len(current_letter_list)
+        list_of_dict.append(current_char_dict)
+    return list_of_dict
+
+
+def num_key(dict):
+    return dict.get("num", None)
+    
+def sort_on(list_of_dictionaries, sort_func):
+    list_of_dictionaries.sort(reverse=True, key=sort_func)
+    return list_of_dictionaries
+
+def report(sorted_list_of_dictionaries, file_path, word_count):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {file_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {word_count} total words")
+    print("--------- Character Count -------")
+    for dict in sorted_list_of_dictionaries:
+        print(f"{dict.get("char")}: {dict.get("num")}")
+
+    return None
+
+# word_counts = word_count("./books/frankenstein.txt")
+# char_counts = char_count("./books/frankenstein.txt")
+# sorted_counts = sort_on(char_counts, num_key)
+# report(sorted_counts, "./books/frankenstein.txt", word_counts)
 
 
